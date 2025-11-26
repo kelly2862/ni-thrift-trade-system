006cd0ddb24bb528feae3e418cbd3ef52d379d1f
kelly2862 <rainkly17@gmail.com>
2025-11-26 23:48:08 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from extensions import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default="user")   # "user" or "admin"
+    status = db.Column(db.String(20), default="active") # active/suspended/deleted
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, pw):
+        self.password_hash = generate_password_hash(pw)
+
+    def check_password(self, pw):
+        return check_password_hash(self.password_hash, pw)
