25465b66dbda49c5baaa44d33a91cd365fc36617
kelly2862 <rainkly17@gmail.com>
2025-11-28 00:12:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = "supersecretkey123"   # change if you want
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    SECRET_KEY = "dev-secret-key"  # change if you want
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
